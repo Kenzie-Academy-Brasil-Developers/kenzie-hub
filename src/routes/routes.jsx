45cff167c4604ea839fc848b2afd06188441bf68
Alexandre Araujo <alexandre01393@gmail.com>
2022-02-22 20:08:56 -0300

    feature: implemented routes

---
+++ src/routes/routes.jsx
@@ -0,0 +1,20 @@
+import {Switch, Route} from 'react-router-dom'
+
+const Routes = () => {
+    <Switch>
+        <Route exact path='/'>
+
+        </Route>
+        <Route path='/login'>
+
+        </Route>
+        <Route path='/signup'>
+
+        </Route>
+        <Route path='/dashboard'>
+
+        </Route>
+    </Switch>
+}
+
+export default Routes
